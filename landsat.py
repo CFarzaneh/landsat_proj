b4193338d7aceb7b3de0fe3569eedf546b36b19d
Cameron <cameron.farzaneh@gmail.com>
2018-10-06 11:53:34 -0400

    Experimenting with different patch sizes

--- landsat.py
+++ landsat.py
@@ -4,26 +4,72 @@ from keras.layers import Dense, Conv2D, Flatten
 from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-samplePath = '/Users/cfarzaneh/Desktop/8class_new60percent/8classes_with_path_dim_9/'
+dataPath = '/home/cameron/Desktop/8class_new60percent/8classes_with_path_dim_'
 
-x = np.load(samplePath+'X_train_patch.npy','r')
-y = np.load(samplePath+'Y_train_patch.npy','r')
+for i in range(0,2):
+	if i == 0:
+		samplePath = dataPath + '5/'
+	elif i == 1:
+		samplePath = dataPath + '9/'
 
-model = Sequential()
-model.add(Conv2D(8,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu', input_shape=(9,9,8)))
-model.add(Conv2D(16,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(Conv2D(32,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(Conv2D(64,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(Conv2D(128,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
-model.add(Flatten())
-model.add(Dense(3200, activation='relu'))
-model.add(Dense(8, activation='softmax'))
+	x = np.load(samplePath+'X_train_patch.npy')
+	y = np.load(samplePath+'Y_train_patch.npy')
 
-model.summary()
+	valLabels = []
+	valSamples = []
+	for file in sorted(os.listdir(samplePath)):
+		if file.startswith('test_label'):
+			valLabels.append(np.load(samplePath+file))
+		elif file.startswith('test_patch'):
+			valSamples.append(np.load(samplePath+file))
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-	optimizer=Adam(lr=0.0001),
-	metrics=['accuracy'])
+	valLabels = np.concatenate(valLabels,axis=0)
+	valSamples = np.concatenate(valSamples,axis=0)
 
-model.fit(x,y,batch_size=10,epochs=2,verbose=1,validation_split=0.2)
+	if i == 0:
+		inputShape = (5,5,8)
+	elif i == 1:
+		inputShape = (9,9,8)
+
+	model = Sequential()
+	model.add(Conv2D(8,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu', input_shape=inputShape))
+	model.add(Conv2D(16,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+	model.add(Conv2D(32,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+	model.add(Conv2D(64,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+	model.add(Conv2D(128,kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+	model.add(Flatten())
+	model.add(Dense(3200, activation='relu'))
+	model.add(Dense(8, activation='softmax'))
+
+	model.summary()
+
+	model.compile(loss=keras.losses.categorical_crossentropy,
+		optimizer=Adam(lr=0.0001),
+		metrics=['accuracy'])
+
+	epochs = 30
+	hs = model.fit(x,y,batch_size=10,epochs=epochs,verbose=1,validation_data=(valSamples,valLabels))
+
+	score = model.evaluate(valSamples,valLabels,verbose=0)
+	print('Test loss:', score[0])
+	print('Test accuracy:', score[1])
+
+	plt.style.use("ggplot")
+	plt.figure()
+	plt.plot(np.arange(0, epochs), hs.history["loss"], label="train_loss")
+	plt.plot(np.arange(0, epochs), hs.history["val_loss"], label="val_loss")
+	plt.plot(np.arange(0, epochs), hs.history["acc"], label="train_acc")
+	plt.plot(np.arange(0, epochs), hs.history["val_acc"], label="val_acc")
+	plt.title("Training Loss and Accuracy")
+	plt.xlabel("Epoch #")
+	plt.ylabel("Loss/Accuracy")
+	plt.legend(loc="upper left")
+
+	if i == 0:
+		plt.savefig("results_5dim.png")
+	elif i == 1:
+		plt.savefig("results_9dim.png")
+
+	model = None
